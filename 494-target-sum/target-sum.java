8ab19598db4363bdce747563eea8d8775c12aadc
Avanthi186 <avanthikapremkumar2005@gmail.com>
2026-08-25 13:33:10 +0530

    Time: 2 ms (99.96%) | Memory: 43.3 MB (62.28%) - LeetSync

---
+++ 494-target-sum/target-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for(int n : nums){
+            sum+= n;
+        }
+        if(Math.abs(target) > sum){
+            return 0;
+        }
+
+        if((sum + target) % 2 != 0){
+
+            return 0;
+        }
+
+        int req = (sum + target)/2;
+
+        int[] dp = new int[req + 1];
+        dp[0] = 1;
+        for(int n : nums){
+            for(int j = req ; j >= n; j--){
+                dp[j] = dp[j] + dp[j - n];
+            }
+        }
+        return dp[req];
+
+    }
+}
